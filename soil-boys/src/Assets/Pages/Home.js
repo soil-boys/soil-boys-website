3fd1ab9fd761ee158e0f7e3cf3a87a47db603f2a
Risuleia <hreethgupta2006@gmail.com>
2023-06-13 16:45:00 +0530

    sections

--- soil-boys/src/Assets/Pages/Home.js
+++ soil-boys/src/Assets/Pages/Home.js
@@ -11,6 +11,13 @@ function Home() {
                     <p className="quote-text">If you can see it, you can shoot it.</p>
                     <span className="quote-end">”</span>
                 </div>
+                <section className="section-1">
+                    <h2 className="heading">What we do</h2>
+                    <div className="content-wrapper">
+                        <span className="side-bar"></span>
+                        <div className="content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum culpa maiores optio vero voluptates eligendi beatae ullam. Iusto nisi impedit officiis reprehenderit? Veniam repellendus facilis repellat temporibus voluptatem dolorem magni!</div>
+                    </div>
+                </section>
             </>
         </div>
     )
