8ba53e106b2af762021e7da130dc968160301cc7
Risuleia <hreethgupta2006@gmail.com>
2023-11-13 07:43:21 +0530

    added animations to latest section

--- src/Assets/Pages/Home.jsx
+++ src/Assets/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import Carousel from "../Structures/Carousel/Carousel";
+import updateDetails from "../../Functions/UpdateDetails";
 
 function Home() {
 
@@ -9,6 +10,12 @@ function Home() {
     const divider = useRef(null)
     const section3 = useRef(null)
     const details = useRef(null)
+
+    const picture1 = useRef(null)
+    const picture2 = useRef(null)
+    const picture3 = useRef(null)
+    const picture4 = useRef(null)
+    const pictureRefArr = [picture1, picture2, picture3, picture4]
 
     const fetchData = async () => {
         try {
@@ -41,9 +48,15 @@ function Home() {
 
         const latestObserver = new IntersectionObserver(entries => {
             entries[0].target.classList.toggle('visible', entries[0].isIntersecting)
-            // details.current.classList.toggle('visible', entries[0].intersectionRatio > 0.25)
-        }, { threshold: 0.2 })
+        }, { threshold: 0.18 })
         latestObserver.observe(section3.current)
+
+        const pictureObserver = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting) updateDetails(entries[0].target, details.current)
+        }, { threshold: 0.4 })
+        pictureRefArr.forEach(ref => {
+            pictureObserver.observe(ref.current)
+        });
 
     }, [])
 
@@ -80,6 +93,37 @@ function Home() {
         },
     ]
 
+    const latestContent = [
+        {
+            id: 0,
+            img: 'https://images.pexels.com/photos/3178786/pexels-photo-3178786.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            post_name: 'Name',
+            post_author: 'Anubhab Baruah',
+            post_equipment: 'fujifilm xt200 with 15-45mm kit lens'
+        },
+        {
+            id: 1,
+            img: 'https://images.pexels.com/photos/3685271/pexels-photo-3685271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            post_name: 'Name',
+            post_author: 'Angad Sankhla',
+            post_equipment: 'canon eos 1500d with 18-55mm lens'
+        },
+        {
+            id: 2,
+            img: 'https://images.pexels.com/photos/3640930/pexels-photo-3640930.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            post_name: 'Name',
+            post_author: 'Krishnav Barman',
+            post_equipment: 'kodak ultramax (exp 2011) on pentax zoom 280p'
+        },
+        {
+            id: 3,
+            img: 'https://images.pexels.com/photos/3563630/pexels-photo-3563630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            post_name: 'Name',
+            post_author: 'Daksh Singh Minhas',
+            post_equipment: 'Canon EOS M50 Mark II with 15-45mm lens'
+        },
+    ]
+
     return(
         <>
             <div id="quote" select="false">
@@ -112,27 +156,29 @@ function Home() {
                     <span className="side-bar"></span>
                     <div className="section-container">
                         <div className="picture-section">
-                                <div className="img-container">
-                                    <img src="https://images.pexels.com/photos/3178786/pexels-photo-3178786.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                                </div>
-                                <div className="img-container">
-                                    <img src="https://images.pexels.com/photos/3685271/pexels-photo-3685271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                                </div>
-                                <div className="img-container">
-                                    <img src="https://images.pexels.com/photos/3640930/pexels-photo-3640930.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                                </div>
-                                <div className="img-container">
-                                    <img src="https://images.pexels.com/photos/3563630/pexels-photo-3563630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" srcSet="  " />
-                                </div>
+                            {
+                                latestContent.map(item => (
+                                    <div
+                                        key={item.id}
+                                        className="img-container"
+                                        ref={pictureRefArr[item.id]}
+                                        data-name={item.post_name}
+                                        data-author={item.post_author}
+                                        data-equip={item.post_equipment}    
+                                    >
+                                        <img src={item.img} alt="" />
+                                    </div>
+                                ))
+                            }
                         </div>
                         <div className="details-section" ref={details}>
                             <div className="details">
-                                <div className="post-name">Name</div>
+                                <div className="post-name" maskable="true">Post Name</div>
                                 <a className="post-credit">
                                     <span className="post-credit-line">@</span>
-                                    <span className="post-credit-author">Anubhab Baruah</span>
+                                    <span className="post-credit-author" maskable="true">Post Author</span>
                                 </a>
-                                <div className="post-equipment"><span>Shot on</span>fujifilm xt200 with 15-45mm kit lens</div>
+                                <div className="post-equipment"><span>Shot on</span><span maskable="true">equipment used</span></div>
                             </div>
                             {/* <div className="details">
                                 <div className="post-name">Name</div>
