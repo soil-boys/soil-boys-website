6a37aa77b87d358767437c901ae743c45826a4c2
Risuleia <hreethgupta2006@gmail.com>
2023-12-21 03:27:07 +0530

    fix off-body whitespace issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Nav from './Assets/Structures/Nav/Nav'
 import Footer from './Assets/Structures/Footer/Footer'
@@ -10,17 +10,28 @@ import './App.css'
 
 function App() {
   
+  const [width, setWidth] = useState('100vw')
+
+  useEffect(() => {
+    setInterval(() => {
+        setWidth(document.body.clientWidth)
+    }, 5000)
+    window.addEventListener('resize', (e) => {
+        setWidth(document.body.clientWidth)
+    })
+  }, [])
+
   return (
-    <>
-      <Nav />
-      <div id='parallax'></div>
-      <div id="main">
-        <AnimatedRoutes />
-      </div>
-      <Footer />
-      {/* <Breadcrumbs /> */}
-    </>
-  )
+        <>
+            <Nav />
+            <div id='parallax' style={{ width: width }}></div>
+            <div id="main">
+                <AnimatedRoutes />
+            </div>
+            <Footer />
+            {/* <Breadcrumbs /> */}
+        </>
+    )
 }
 
 export default App
